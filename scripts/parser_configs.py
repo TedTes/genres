aa014bf72d096b641fbbe1b93bc9e542b78b5c66
Tedros <tedtfu@gmail.com>
2025-04-10 10:58:48 -0400

    added configs for parsing, scraping

--- scripts/parser_configs.py
+++ scripts/parser_configs.py
@@ -7,7 +7,9 @@ configs = {
         "company": {"selector": ".job-logo img[alt]", "extract": "alt"},
         "location": {"selector": ".job-data h4", "extract": "text"},
         "job_id": {"selector": ".listing-attributes .job-attribute:nth-child(1)", "extract": "text"},
-        "type": {"selector": ".listing-attributes .job-attribute:nth-child(2)", "extract": "text"}
+        "type": {"selector": ".listing-attributes .job-attribute:nth-child(2)", "extract": "text"},
+        "description_selector": ".job-detail",
+        "apply_selector": ".applyNowBtn"
     },
     "s-buck": {
         "site": "https://starbucks.eightfold.ai/careers",
@@ -17,17 +19,9 @@ configs = {
         "company": {"value": "Starbucks"},
         "location": {"selector": ".position-location", "extract": "text"},
         "job_id": {"value": None},
-        "type": {"selector": ".position-priority-container", "extract": "text"}
-    },
-    "t-hort": {
-        "site": "https://app.higherme.com/careers/58bd9e7f472bd?langCode=en",
-        "container_selector": "div[class='hCXxvRc89YBhPXkzL9Wl'] > div",
-        "url": {"selector": "button.sc-jbAkgO[href]", "extract": "href"},
-        "title": {"selector": "h1.sc-gjcoXW", "extract": "text"},
-        "company": {"value": "Tim Hortons"},
-        "location": {"selector": ".e5KiSkcjp_YiIIkHDmU3", "extract": "text"},
-        "job_id": {"value": None},
-        "type": {"selector": ".sc-ezeoWd span", "extract": "text", "multiple": True}
+        "type": {"selector": ".position-priority-container", "extract": "text"},
+        "description_selector": ".position-details > .row",  # Fixed selector
+        "apply_selector": "button[type='button']"  # Try a common apply button selector
     },
     "w-mart": {
         "site": "https://careers.walmart.ca/search-jobs",
@@ -37,7 +31,9 @@ configs = {
         "company": {"value": "Walmart"},
         "location": {"selector": ".job-location", "extract": "text", "multiple": True},
         "job_id": {"selector": "self", "extract": "data-job-id"},
-        "type": {"selector": ".job-info.job-category", "extract": "text"}
+        "type": {"selector": ".job-info.job-category", "extract": "text"},
+        "description_selector": ".job-description",  # Simplified selector
+        "apply_selector": ".job-apply"  # Simplified selector
     },
     "l-law": {
         "site": "https://careers.loblaw.ca/jobs",
@@ -46,17 +42,9 @@ configs = {
         "title": {"selector": ".results-list__item-title span", "extract": "text"},
         "company": {"selector": ".results-list__item-brand--label", "extract": "text"},
         "location": {"selector": ".results-list__item-street--label", "extract": "text"},
-        "job_id": {"selector": "self", "extract": "data-testid", "post_process": "split('_')[-1]"}
-    },
-    "j-bank": {
-        "site": "https://www.jobbank.gc.ca/jobsearch/",
-        "container_selector": ".results-jobs",
-        "url": {"selector": ".resultJobItem[href]", "extract": "href"},
-        "title": {"selector": ".noctitle", "extract": "text"},
-        "company": {"selector": ".business", "extract": "text"},
-        "location": {"selector": ".location", "extract": "text"},
-        "job_id": {"selector": "self", "extract": "id", "post_process": "split('-')[-1]"},
-        "type": {"selector": ".telework", "extract": "text"}
+        "job_id": {"selector": "self", "extract": "data-testid", "post_process": "split('_')[-1]"},
+        "description_selector": ".main-content",
+        "apply_selector": ".apply-button, button[type='submit']"  # Try common apply buttons
     },
     "m-don": {
         "site": "https://careers.mcdonalds.ca/restaurant-jobs",
@@ -65,16 +53,20 @@ configs = {
         "title": {"selector": ".results-list__item-title span", "extract": "text"},
         "company": {"selector": ".results-list__item-brand--label", "extract": "text"},
         "location": {"selector": ".results-list__item-street--label", "extract": "text"},
-        "job_id": {"selector": ".results-list__item-title[href]", "extract": "href", "post_process": "split('/')[-1]"}
+        "job_id": {"selector": ".results-list__item-title[href]", "extract": "href", "post_process": "split('/')[-1]"},
+        "description_selector": ".content",
+        "apply_selector": ".apply-now, .apply-button"  # Try common apply buttons
     },
-    "m-tro": {
-        "site": "https://talent.metro.ca/en",
-        "container_selector": ".grid.grid-cols-1.gap-4",
-        "url": {"selector": "button.bg-primary[href]", "extract": "href"},
-        "title": {"selector": "h2", "extract": "text"},
-        "company": {"value": "Metro"},
-        "location": {"selector": "ul li:nth-child(1) span:nth-child(2)", "extract": "text"},
+     "t-hort": {
+        "site": "https://app.higherme.com/careers/58bd9e7f472bd?langCode=en",
+        "container_selector": "div[class='hCXxvRc89YBhPXkzL9Wl'] > div",
+        "url": {"selector": "button.sc-jbAkgO[href]", "extract": "href"},
+        "title": {"selector": "h1.sc-gjcoXW", "extract": "text"},
+        "company": {"value": "Tim Hortons"},
+        "location": {"selector": ".e5KiSkcjp_YiIIkHDmU3", "extract": "text"},
         "job_id": {"value": None},
-        "type": {"selector": "ul li:nth-child(2) span:nth-child(2)", "extract": "text"}
+        "type": {"selector": ".sc-ezeoWd span", "extract": "text", "multiple": True},
+        "description_selector" :".oz62RleGS6_qRlv03AmQ",
+        "apply_selector": "sc-jbAkgO.hQrdnR"
     }
 }
