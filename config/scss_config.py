90267bc31de1056b392b274f3fcefa9ae3dfa0e3
Tedros <tedtfu@gmail.com>
2025-04-30 06:28:58 -0400

    scss to css config

---
+++ config/scss_config.py
@@ -0,0 +1,52 @@
+scss_config = {
+    'classic': {
+        'html': 'classic.html',
+        'css': 'scss/layouts/classic.scss',
+        'output_css': 'css/layouts/classic.css'
+    },
+    'modern': {
+        'html': 'modern.html',
+        'css': 'scss/layouts/modern.scss',
+        'output_css': 'css/layouts/modern.css'
+    },    
+    'professional': {
+        'html': 'professional.html',
+        'css': 'scss/layouts/professional.scss',
+        'output_css': 'css/layouts/professional.css'
+    },
+    'cards': {
+        'html': 'cards.html',
+        'css': 'scss/layouts/cards.scss',
+        'output_css': 'css/layouts/cards.css'
+    },
+    'compact': {
+        'html': 'compact.html',
+        'css': 'scss/layouts/compact.scss',
+        'output_css': 'css/layouts/compact.css'
+    },
+    'grid': {
+        'html': 'grid.html',
+        'css': 'scss/layouts/grid.scss',
+        'output_css': 'css/layouts/grid.css'
+    },    
+    'minimalist': {
+        'html': 'minimalist.html',
+        'css': 'scss/layouts/minimalist.scss',
+        'output_css': 'css/layouts/minimalist.css'
+    },
+    'portfolio': {
+        'html': 'portfolio.html',
+        'css': 'scss/layouts/portfolio.scss',
+        'output_css': 'css/layouts/portfolio.css'
+    },
+    'sidebar': {
+        'html': 'sidebar.html',
+        'css': 'scss/layouts/sidebar.scss',
+        'output_css': 'css/layouts/sidebar.css'
+    },
+    'timeline': {
+        'html': 'timeline.html',
+        'css': 'scss/layouts/timeline.scss',
+        'output_css': 'css/layouts/timeline.css'
+    }
+}
