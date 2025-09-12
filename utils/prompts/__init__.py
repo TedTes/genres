938d13bf91f135c9a14570eed61112375a5b5118
Tedros <tedtfu@gmail.com>
2025-09-12 19:28:27 -0400

    bug fixs

--- utils/prompts/__init__.py
+++ utils/prompts/__init__.py
@@ -1,3 +1,3 @@
 from .resume_prompt import create_normalization_prompt,create_json_prompt
-from .optimization_prompt import optimization_prompt
-__all__ = ["create_normalization_prompt","create_json_prompt","optimization_prompt"]
+from .optimization_prompt import get_optimization_prompt
+__all__ = ["create_normalization_prompt","create_json_prompt","get_optimization_prompt"]
