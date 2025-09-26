98c5ae51c4d77ea86fdff204dfab5599ec4743a5
Tedros <tedtfu@gmail.com>
2025-09-25 21:21:00 -0400

    add Template Resolution System

---
+++ services/template/__init__.py
@@ -0,0 +1,5 @@
+from .template_loader import TemplateLoader
+from .template_resolver import TemplateResolver
+from .template_context import TemplateContext
+
+__all__ = ['TemplateLoader', 'TemplateResolver', 'TemplateContext']
